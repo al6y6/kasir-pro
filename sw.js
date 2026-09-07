5fd9396d20f7229883772656013c5c8470f1aeb1
al6y6 <almunjiat@gmail.com>
2026-09-07 09:57:55 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Google Spreadsheet lewat fetch() di index.html. Ini hanya meng-cache file
 // aplikasi (HTML/CSS/JS) agar bisa dibuka walau koneksi lemah.
 
-var CACHE_NAME = 'kasirpro-shell-v6';
+var CACHE_NAME = 'kasirpro-shell-v7';
 var SHELL_FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', function (event) {
